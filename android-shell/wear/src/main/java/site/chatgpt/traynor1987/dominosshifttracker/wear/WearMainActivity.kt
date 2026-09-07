cc819999588e5b53d8982a366ecb75db5bc4d111
James Traynor <Jtraynor1987@aol.com>
2026-09-07 01:06:47 +0100

    Fix View import for main timer label

--- android-shell/wear/src/main/java/site/chatgpt/traynor1987/dominosshifttracker/wear/WearMainActivity.kt
+++ android-shell/wear/src/main/java/site/chatgpt/traynor1987/dominosshifttracker/wear/WearMainActivity.kt
@@ -13,6 +13,7 @@ import android.os.Looper
 import android.os.SystemClock
 import android.provider.Settings
 import android.text.TextUtils
+import android.view.View
 import android.view.Gravity
 import android.view.HapticFeedbackConstants
 import android.view.InputDevice
